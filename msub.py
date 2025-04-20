7d534687e8bd67a241410baca22f873871e03d0e
Project Dev Community Github <158807510+PCMEVNCODE@users.noreply.github.com>
2025-04-20 21:44:50 +0700

    msub.py

---
+++ msub.py
@@ -0,0 +1,20 @@
+# FILE CODE SUB FOR MAIN FILE
+# <DEVLOPER> >> DANH DEV
+#
+#openrouter
+#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
+
+# id token for discord bot _>>>
+tapi = "MTM2MzE0NTA2NzQ4OTAwNTc1OQ.Grr-W8.z4oCPY8v7WQszlXF8FJ-NHVdaHf-MLIMo1SqjY"
+#____________________________________________________________________________________________
+# CHANNEL_ID >>>>
+CH_ID = 1363158722242805841
+#____________________________________________________________________________________________
+# FREFIX TO READ MESSAGE >>>>
+FREFIX = "<<"
+#____________________________________________________________________________________________
+#api_gemini
+a_ge = "AIzaSyCR6laHkZeWHVSiEx5l-vsYa8BfU-Mhseg"
+#____________________________________________________________________________________________
+# API for deepseek
+d_api = "sk-ee28221a8a7c47e2b700879218cce84d"
