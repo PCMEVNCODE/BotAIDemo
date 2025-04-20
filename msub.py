4db72be49bda090b35d0a245363bb5ce38069863
Project Dev Community Github <158807510+PCMEVNCODE@users.noreply.github.com>
2025-04-21 05:13:26 +0700

    msub.py

--- msub.py
+++ msub.py
@@ -3,9 +3,9 @@
 #
 #openrouter
 #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
-
+import os
 # id token for discord bot _>>>
-tapi = "MTM2MzE0NTA2NzQ4OTAwNTc1OQ.GXK1cI.e430Gn1qp-NCbeYxlKG865axhnP2SQAB_g8LIY"
+tapi = os.getenv("token_b")
 #____________________________________________________________________________________________
 # CHANNEL_ID >>>>
 CH_ID = 1363158722242805841
@@ -14,7 +14,7 @@ CH_ID = 1363158722242805841
 FREFIX = "<<"
 #____________________________________________________________________________________________
 #api_gemini
-a_ge = "AIzaSyCR6laHkZeWHVSiEx5l-vsYa8BfU-Mhseg"
+a_ge = os.getenv("a_ge") 
 #____________________________________________________________________________________________
 # API for deepseek
 d_api = "sk-ee28221a8a7c47e2b700879218cce84d"
